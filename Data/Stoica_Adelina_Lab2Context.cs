13e2288e85fc48f1e2fb21705109a97d4338b4b7
adelinastoica <adestoica18@gmail.com>
2022-11-19 16:16:29 +0200

    Add project files.

---
+++ Data/Stoica_Adelina_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stoica_Adelina_Lab2.Models;
+
+namespace Stoica_Adelina_Lab2.Data
+{
+    public class Stoica_Adelina_Lab2Context : DbContext
+    {
+        public Stoica_Adelina_Lab2Context (DbContextOptions<Stoica_Adelina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stoica_Adelina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Stoica_Adelina_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Stoica_Adelina_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Stoica_Adelina_Lab2.Data;
+using Stoica_Adelina_Lab2.Models;
+
+namespace Stoica_Adelina_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Stoica_Adelina_Lab2.Data.Stoica_Adelina_Lab2Context _context;
+
+        public CreateModel(Stoica_Adelina_Lab2.Data.Stoica_Adelina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID","PublisherName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Models.Author>(), "ID", "FullName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
